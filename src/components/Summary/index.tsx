290c66e75fa9aed98a1bcdb11f7ff2811904fbf3
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2023-12-14 21:51:56 -0300

    feat: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,31 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>R$ 6.800,00</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>R$ 2.300,00</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+        <strong>R$ 4.500,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
