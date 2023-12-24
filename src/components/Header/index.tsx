64334249bb60bec58e6bf8e80ddef93b208ee665
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2023-12-23 21:25:34 -0300

    feat: Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
 import logoImg from '../../assets/logo.svg'
+
+import * as Dialog from '@radix-ui/react-dialog'
+
+import { NewTransactionModal } from '../NewTransactionModal';
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton> Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
-    </HeaderContainer>
+    </HeaderContainer >
   )
 }
