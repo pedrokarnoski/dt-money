8a2e5356973e312c14abacfc6cdd67176ee6ae78
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-01-13 19:14:10 -0300

    feat: Requests

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react"
+
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
 import { SearchForm } from "./components/SearchForm"
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/transactions')
+      .then((response) => response.json())
+      .then(data => {
+        setTransactions(data)
+      })
+  }, [])
+
   return (
     <div>
       <Header />
@@ -14,25 +35,21 @@ export function Transactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 12.000,00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>14/12/2023</td>
-            </tr>
-            <tr>
-              <td width="50%">Hambúrguer</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                  - R$ 59,00
-                </PriceHighlight></td>
-              <td>Alimentação</td>
-              <td>12/12/2023</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })
+            }
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
